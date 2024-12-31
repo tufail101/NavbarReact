196ddf47f6168f1dadda4b125deb0c3594890125
Tufail Ansari <thebinaryblasters@gmail.com>
2024-12-31 21:58:42 +0530

    >>

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import About from './component/About'
+import Contact from './component/Contact'
+import Home from './component/home'
+import Navbar from './component/Navbar'
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+      
+      
+      </Routes>
+      </Router>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Contact.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Contact() {
+  return (
+    <div>
+      i am contact Page
+    </div>
+  )
+}
